11efa71e860359829b6a28fda576f001f9efdbe9
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-04-16 16:15:36 +1200

    problem solving drop down

--- website.js
+++ website.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', showSlides)
+document.addEventListener('DOMContentLoaded', start)
 
+function start(){
+/*
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -14,12 +16,11 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
@@ -27,4 +28,12 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}*/
+
+$(document).ready(function(){
+  $(".dropdown-toggle").on("click", function(e){
+    $(this).next('li').toggle();
+  });
+});
+
 }
